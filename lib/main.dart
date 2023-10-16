8b4180d2715e7fa955418e09c34c5c3d11209787
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-16 13:58:31 -0700

    62 Rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text('Rows and Columns'),
+      ),
+      body: MyWidget(),
+      )
+    )
+  );
 }
+
+
+class MyWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Center(child:Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Container(
+        width:100, 
+        height: 100, 
+        color:Colors.red[200]),
+        Container(
+        width:100, 
+        height: 100, 
+        color:Colors.green[200]),
+        Container(
+        width:100, 
+        height: 100, 
+        color:Colors.yellow[200]),
+        ]
+    ));
+  }
+}
+
