4699739278cba32158cfc934174ab81115d3480b
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-16 14:32:29 -0700

    63 - stateful widget part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-        title: const Text('Rows and Columns'),
-      ),
-      body: MyWidget(),
-      )
-    )
-  );
-}
+void main() => runApp(MaterialApp(home: MyWidget()));
 
-
-class MyWidget extends StatelessWidget{
-
+class MyWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(child:Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Container(
-        width:100, 
-        height: 100, 
-        color:Colors.red[200]),
-        Container(
-        width:100, 
-        height: 100, 
-        color:Colors.green[200]),
-        Container(
-        width:100, 
-        height: 100, 
-        color:Colors.yellow[200]),
-        ]
-    ));
-  }
+  Widget build(BuildContext context) =>
+      Scaffold(body: Container(color: Colors.deepPurple[700]));
 }
-
