7a7984bcb986a95ae722cddbfb4a4801fd234ab9
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-16 18:36:10 -0700

    64 stateful widget pppart 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: MyWidget()));
+void main() => runApp(MaterialApp(home: MyHomePage()));
 
-class MyWidget extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(body: Container(color: Colors.deepPurple[700]));
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //initial variable
+  bool isButtonPressed = false;
+
+  //setState()?if
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: GestureDetector(onTap: () {
+      if (isButtonPressed) {
+        setState(() {
+          isButtonPressed = false;
+        });
+      } else {
+        setState(() {
+          isButtonPressed = true;
+        });
+      }
+    },
+    
+    child: Container,
+    
+    
+    
+    
+    ));
+  }
 }
