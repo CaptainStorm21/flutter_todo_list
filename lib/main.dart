a2c6fae485991ad8bb05d06711f40220bc419f0c
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-16 23:58:43 -0700

    66 text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,46 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(home: Scaffold(body: Center(child: Counter()))));
+void main() => runApp(MyApp());
 
-class Counter extends StatefulWidget {
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  
-  //Row
-  //Button Counter: 4
-
-
-
-  //initial value _counter var
-  int _counter = 0;
-  void _increment() {
-    setState(() {
-      //update counter
-      _counter++;
-    });
-  }
-
-  //widgets build() button on pressed: _increment
+//stateless myapp
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextButton(
-          onPressed: _increment,
-          child: const Text('Push Me!')
-        ), 
-        Text('You Pressed $_counter times')],
-        );
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+//statefull myhomepage
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //initial value var
+  String _inputText = '';
+
+  //widget --> setState() --> update Var -- Widget State
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('An App Bar')),
+        body: Center(
+          child: TextField(
+            decoration: InputDecoration(hintText: "Enter some text"),
+            onChanged: (value) {
+              //onPressed === anytime you update the text the value will get passed in
+              setState(() {
+                _inputText = value;
+              });
+            },
+          ),
+        ),
+        bottomSheet: Container(
+          color: Colors.blueGrey[400],
+          alignment: Alignment.center,
+          height: 100,
+          child: Text('You have entered $_inputText'),
+        ));
   }
 }
