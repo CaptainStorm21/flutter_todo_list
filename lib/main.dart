1ddf5162b58ee1c66c4f1d41c3ee78767b172315
Captain Storm <CaptainStorm21@users.noreply.github.com>
2023-10-17 23:36:48 -0700

    68 ListView part1

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyDialog());
+void main() => runApp(MyListView());
 
-class MyDialog extends StatelessWidget {
+class MyListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Dialog demo', home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // initial var - string
-  String _inputText = '';
-  // void show input dialog -- alertbox
-  void _showInputDialog() {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          TextEditingController controller = TextEditingController();
-          return AlertDialog(
-            title: Text("Enter some text"),
-            content: TextField(
-              controller: controller,
-              decoration: InputDecoration(hintText: 'Enter some text'),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('ListView')),
+          body: ListView(children: [
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Person One'),
+              subtitle: Text('Animals@yahoo.com'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () => print('hellow world'),
             ),
-            actions: <Widget>[
-              TextButton(
-                child: Text('Cancel'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              ),
-              TextButton(
-                child: Text('Save'),
-                onPressed: () {
-                  setState(() {
-                    _inputText = controller.text;
-                  });
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-  }
-
-// Return Scaffold
-// appBr body update State text - Icon
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('My New App')),
-      body: Center(
-        child: Text(
-            // result = textCondition ? truevalue : falsevalue
-            _inputText.isEmpty ? 'Tap the Icon' : '$_inputText'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showInputDialog,
-        child: Icon(Icons.edit),
-      ),
+                        ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Person One'),
+              subtitle: Text('Animals@yahoo.com'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () => print('hellow world'),
+            ),
+                        ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Person One'),
+              subtitle: Text('Animals@yahoo.com'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () => print('hellow world'),
+            ),
+          ])),
     );
   }
 }
-
-// _input text empty or not  == '' .isEmpty
-// if .isEmpty is true the "tap the icon to enter text"
-//ellse "you entered $_inputtext"
-// result = textCondition ? truevalue : falsevalue
